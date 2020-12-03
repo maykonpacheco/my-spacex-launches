cfb4f7966fac5ee6ff84063069d4d5aa5ae664a8
Maykon Pacheco <maykon.albuquerque@datweb.com.br>
2020-12-03 09:00:20 -0400

    title change

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ launches }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>SpaceX Launches</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
